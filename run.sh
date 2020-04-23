1d3240b9b2f01dfda25d60692e1e83776f894fb4
yifengjianbaiwl <617306046@qq.com>
2020-04-23 14:02:31 +0800

    -0-0

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ echo "start run shell"
 dotnet build
 dotnet publish -o /var/lib/jenkins/workspace/publish
 cd /etc/supervisor
-echo "change to etc/supervisor"
-pwd
+echo "开始重启守护进程"
 supervisorctl reload
 echo "success finish shell"
