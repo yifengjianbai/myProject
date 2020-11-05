57c0f85762fbfa8f2851638ec99a1f60ff9975c3
yifengjianbaiwl <617306046@qq.com>
2020-11-05 11:55:44 +0800

    --

--- OpenAuth.App/ReptilesApp.cs
+++ OpenAuth.App/ReptilesApp.cs
@@ -76,10 +76,16 @@ namespace OpenAuth.App
                         {
                             lastF191 = model.data.f191;
                         }
+
+                        if (model.data.f170 != null && model.data.f191 != null)
+                        {
+                            continue;
+                        }
+
                         Ticksanalysis ticksanalysis = new Ticksanalysis()
                         {
-                            Code=code,
-                            Time=DateTime.Now,
+                            Code = code,
+                            Time = DateTime.Now,
                             WeiBi = Convert.ToSingle(lastF191),
                             UpDown = Convert.ToSingle(lastF170)
                         };
@@ -102,7 +108,7 @@ namespace OpenAuth.App
             try
             {
                 IsBegin = true;
-                HttpGet("http://51.push2.eastmoney.com/api/qt/stock/sse?ut=fa5fd1943c7b386f172d6893dbfba10b&fltt=2&fields=f120,f121,f122,f174,f175,f59,f163,f43,f57,f58,f169,f170,f46,f44,f51,f168,f47,f164,f116,f60,f45,f52,f50,f48,f167,f117,f71,f161,f49,f530,f135,f136,f137,f138,f139,f141,f142,f144,f145,f147,f148,f140,f143,f146,f149,f55,f62,f162,f92,f173,f104,f105,f84,f85,f183,f184,f185,f186,f187,f188,f189,f190,f191,f192,f107,f111,f86,f177,f78,f110,f262,f263,f264,f267,f268,f255,f256,f257,f258,f127,f199,f128,f198,f259,f260,f261,f171,f277,f278,f279,f288,f152,f250,f251,f252,f253,f254,f269,f270,f271,f272,f273,f274,f275,f276,f265,f266,f289,f290,f294,f295&secid=1.600764",600764);
+                HttpGet("http://51.push2.eastmoney.com/api/qt/stock/sse?ut=fa5fd1943c7b386f172d6893dbfba10b&fltt=2&fields=f120,f121,f122,f174,f175,f59,f163,f43,f57,f58,f169,f170,f46,f44,f51,f168,f47,f164,f116,f60,f45,f52,f50,f48,f167,f117,f71,f161,f49,f530,f135,f136,f137,f138,f139,f141,f142,f144,f145,f147,f148,f140,f143,f146,f149,f55,f62,f162,f92,f173,f104,f105,f84,f85,f183,f184,f185,f186,f187,f188,f189,f190,f191,f192,f107,f111,f86,f177,f78,f110,f262,f263,f264,f267,f268,f255,f256,f257,f258,f127,f199,f128,f198,f259,f260,f261,f171,f277,f278,f279,f288,f152,f250,f251,f252,f253,f254,f269,f270,f271,f272,f273,f274,f275,f276,f265,f266,f289,f290,f294,f295&secid=1.600764", 600764);
             }
             catch (Exception ex)
             {
